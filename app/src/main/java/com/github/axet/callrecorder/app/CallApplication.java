f4b475c5bb147fed22fc53509f7aa881ed528c6a
Alexey Kuznetsov <axet@me.com>
2019-11-16 10:00:26 +0300

    fix code cache assets names

--- app/src/main/java/com/github/axet/callrecorder/app/CallApplication.java
+++ app/src/main/java/com/github/axet/callrecorder/app/CallApplication.java
@@ -10,10 +10,9 @@ import android.os.Build;
 import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 
-import com.github.axet.androidlibrary.app.AssetsDexLoader;
 import com.github.axet.androidlibrary.app.NotificationManagerCompat;
-import com.github.axet.androidlibrary.widgets.NotificationChannelCompat;
 import com.github.axet.androidlibrary.preferences.OptimizationPreferenceCompat;
+import com.github.axet.androidlibrary.widgets.NotificationChannelCompat;
 import com.github.axet.callrecorder.R;
 
 import java.lang.reflect.Method;
@@ -69,8 +68,6 @@ public class CallApplication extends com.github.axet.audiolibrary.app.MainApplic
         channelStatus = new NotificationChannelCompat(this, "status", "Status", NotificationManagerCompat.IMPORTANCE_LOW);
 
         OptimizationPreferenceCompat.setPersistentServiceIcon(this, true);
-
-        AssetsDexLoader.clearCodeCacheDir(this);
 
         switch (getVersion(PREFERENCE_VERSION, R.xml.pref_general)) {
             case -1:
