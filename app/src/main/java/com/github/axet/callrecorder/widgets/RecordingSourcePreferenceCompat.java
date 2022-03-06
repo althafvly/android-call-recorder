7bee7910a50a42efe93c324f30a6837795ee322b
Alexey Kuznetsov <axet@me.com>
2022-03-06 20:29:35 +0300

    move rec pref to library

--- app/src/main/java/com/github/axet/callrecorder/widgets/RecordingSourcePreferenceCompat.java
+++ app/src/main/java/com/github/axet/callrecorder/widgets/RecordingSourcePreferenceCompat.java
@@ -24,7 +24,7 @@ import com.github.axet.callrecorder.services.VoiceRecognitionService;
 
 import java.util.LinkedHashMap;
 
-public class RecordingSourcePreferenceCompat extends ListPreference {
+public class RecordingSourcePreferenceCompat extends com.github.axet.audiolibrary.widgets.RecordingSourcePreferenceCompat {
     public static final String TAG = RecordingSourcePreferenceCompat.class.getSimpleName();
 
     public static boolean isEnabled(Context context) {
@@ -56,75 +56,23 @@ public class RecordingSourcePreferenceCompat extends ListPreference {
 
     public RecordingSourcePreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        create();
     }
 
     public RecordingSourcePreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        create();
     }
 
     public RecordingSourcePreferenceCompat(Context context, AttributeSet attrs) {
         super(context, attrs);
-        create();
     }
 
     public RecordingSourcePreferenceCompat(Context context) {
         super(context);
-        create();
-    }
-
-    LinkedHashMap<String, String> getSources() {
-        CharSequence[] text = getEntries();
-        CharSequence[] values = getEntryValues();
-        LinkedHashMap<String, String> mm = new LinkedHashMap<>();
-        for (int i = 0; i < values.length; i++) {
-            String v = values[i].toString();
-            String t = text[i].toString();
-            mm.put(v, t);
-        }
-        return mm;
-    }
-
-    public boolean isSourceSupported(int s) {
-        if (s == MediaRecorder.AudioSource.UNPROCESSED && !Sound.isUnprocessedSupported(getContext()))
-            return false;
-        return true;
-    }
-
-    public LinkedHashMap<String, String> filter(LinkedHashMap<String, String> mm) {
-        LinkedHashMap<String, String> map = new LinkedHashMap<>();
-        for (String v : mm.keySet()) {
-            String t = mm.get(v);
-            Integer s = Integer.parseInt(v);
-            if (!isSourceSupported(s))
-                continue;
-            map.put(v, t);
-        }
-        return map;
-    }
-
-    public void setValues(LinkedHashMap<String, String> mm) {
-        String v = getValue();
-        if (mm.size() > 1) {
-            setEntryValues(mm.keySet().toArray(new CharSequence[0]));
-            setEntries(mm.values().toArray(new CharSequence[0]));
-            int i = findIndexOfValue(v);
-            if (i == -1)
-                setValueIndex(0);
-            else
-                setValueIndex(i);
-        } else {
-            setVisible(false);
-        }
-        update(v); // defaultValue null after defaults set
     }
 
     public void create() {
+        super.create();
         setWidgetLayoutResource(R.layout.accessibilityservice);
-        LinkedHashMap<String, String> mm = getSources();
-        mm = filter(mm);
-        setValues(mm);
     }
 
     @Override
@@ -147,33 +95,5 @@ public class RecordingSourcePreferenceCompat extends ListPreference {
         } else {
             view.setVisibility(View.GONE);
         }
-    }
-
-    @Override
-    public boolean callChangeListener(Object newValue) {
-        update(newValue);
-        return super.callChangeListener(newValue);
-    }
-
-    @Override
-    protected Object onGetDefaultValue(TypedArray a, int index) {
-        Object def = super.onGetDefaultValue(a, index);
-        update(def);
-        return def;
-    }
-
-    @Override
-    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-        super.onSetInitialValue(restoreValue, defaultValue);
-        update(getValue()); // defaultValue null after defaults set
-    }
-
-    public void update(Object value) {
-        String v = (String) value;
-        setSummary(v);
-    }
-
-    public void onResume() {
-        callChangeListener(getValue());
     }
 }
